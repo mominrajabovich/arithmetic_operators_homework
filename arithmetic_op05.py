61fef57af5ce49bf909505984f78aa4a66818143
momin <mominbabajanov@gmail.com>
2022-12-27 16:18:57 +0500

    05

--- arithmetic_op05.py
+++ arithmetic_op05.py
@@ -1,6 +1,7 @@
 #Create a variable called 'number' with data type int.
 number=13
-#Create a variable called 'answer' and assign the remainder of the division of number by 3 to it.
-answer=number/3
+#Create a variable called 'answer'
+#  and assign the remainder of the division of number by 3 to it.
+answer=number%3
 #Print 'answer'.
 print(answer)
